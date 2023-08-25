ba86769cc741557ae8a6159fb7277224154a0c1a
Ivan-Lysenko <diparon@yandex.ru>
2023-08-25 16:54:27 +0300

    Refactor genDiff function

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -1,41 +1,47 @@
 <?php
 namespace Php\Project\GenDiff;
 
-function genDiff($args)
+function run($args)
 {
     $pathToFirstFile = $args['<firstFile>'];
     $pathToSecondFile = $args['<secondFile>'];
 
-    $firstFile = json_decode(file_get_contents($pathToFirstFile), true);
-    $secondFile = json_decode(file_get_contents($pathToSecondFile), true);
-    //var_dump($firstFile);
-    boolValueDisplay($firstFile);
-    boolValueDisplay($secondFile);
+    echo genDiff($pathToFirstFile, $pathToSecondFile);
+}
 
-    //var_dump($firstFile);
-    
-    $merged = array_merge($firstFile, $secondFile);
-    ksort($merged);
-    //var_dump($merged);
+function genDiff($firstFile, $secondFile)
+{
+
+    $firstFileData = json_decode(file_get_contents($firstFile), true);
+    $secondFileData = json_decode(file_get_contents($secondFile), true);
+    //TODO: стоит попробовать использовать var_export($value, true)
+    boolValueDisplay($firstFileData);
+    boolValueDisplay($secondFileData);
+
+    $mergedFile = array_merge($firstFileData, $secondFileData);
+    ksort($mergedFile);
+
     $result = "{\n";
-    foreach ($merged as $key => $value) {
-        if (array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile)) {
-            if ($firstFile[$key] === $secondFile[$key]) {
-                $result .= "  {$key}: {$value}\n";
+        
+    foreach ($mergedFile as $key => $value) {
+        if (array_key_exists($key, $firstFileData) && array_key_exists($key, $secondFileData)) {
+            if ($firstFileData[$key] === $secondFileData[$key]) {
+                $result .= "    {$key}: {$value}\n";
             } else {
-                $result .= "- {$key}: {$firstFile[$key]}\n";
-                $result .= "+ {$key}: {$secondFile[$key]}\n";
+                $result .= "  - {$key}: {$firstFileData[$key]}\n";
+                $result .= "  + {$key}: {$secondFileData[$key]}\n";
             }
         }
-        if (array_key_exists($key, $firstFile) && !array_key_exists($key, $secondFile)) {
-            $result .= "- {$key}: {$firstFile[$key]}\n";
+        if (array_key_exists($key, $firstFileData) && !array_key_exists($key, $secondFileData)) {
+            $result .= "  - {$key}: {$firstFileData[$key]}\n";
         }
-        if (!array_key_exists($key, $firstFile) && array_key_exists($key, $secondFile)) {
-            $result .= "+ {$key}: {$secondFile[$key]}\n";
+        if (!array_key_exists($key, $firstFileData) && array_key_exists($key, $secondFileData)) {
+            $result .= "  + {$key}: {$secondFileData[$key]}\n";
         }
     }
     $result .= "}";
-    echo $result;
+
+    return $result;
 }
 
 function boolValueDisplay(&$arr)
